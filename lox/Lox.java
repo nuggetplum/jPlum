32fb77ad3d76537f32b816fd2440d7c5d0bf7cc1
Aditya K <notaditya2k4@gmail.com>
2026-09-03 14:06:08 +0530

    error handling

--- lox/Lox.java
+++ lox/Lox.java
@@ -7,6 +7,7 @@ import java.nio.charset.Charset;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.util.List;
+import java.util.Scanner;
 
 public class Lox {
     public static void main(String[] args) throws IOException {
@@ -35,5 +36,24 @@ public class Lox {
                 break;
             run(line);
         }
+    }
+
+    private static void run(String source) {
+        Scanner scanner = new Scanner(source);
+        List<Token> tokens = scanner.scanTokens();
+        for (Token token : tokens) {
+            System.out.println(token);
+        }
+    }
+
+    static void error(int line, String message) {
+        report(line, "", message);
+    }
+
+    private static void report(int line, String where,
+            String message) {
+        System.err.println(
+                "[line " + line + "] Error" + where + ": " + message);
+        hadError = true;
     }
 }
