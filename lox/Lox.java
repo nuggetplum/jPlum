fa69eada5f4a7191df37b6acbb29e8833bfe8d99
Aditya K <notaditya2k4@gmail.com>
2026-09-20 01:43:41 +0530

    error lmao, all for the greens

--- lox/Lox.java
+++ lox/Lox.java
@@ -60,7 +60,7 @@ public class Lox {
     private static void report(int line, String where,
             String message) {
         System.err.println(
-                "[line " + line + "] Error" + where + ": " + message);
+                "[line " + line + "] Erro" + where + ": " + message);
         hadError = true;
     }
 }
